7a4c419053e4257c25fdc00c65ed5cfd230fa3f5
Travis Askham <askhamwhat@gmail.com>
2017-03-02 15:50:25 -0500

    add driver that calls all data-generating scripts

---
+++ examples/run_all_examples.m
@@ -0,0 +1,13 @@
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% Run through all examples and generate data
+%
+% THIS MAY TAKE A LONG TIME
+%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+fprintf('generating data for periodic example ...')
+sensor_periodic
+fprintf('generating data for hidden dynamics example ...')
+sensor_hidden_dynamics
